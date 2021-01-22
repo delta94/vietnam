0de98f95362f9db96e508519476060a3511ccd72
harry4 <harry@boostcommerce.net>
2021-01-22 17:58:44 +0700

    register

--- web/react/src/index.tsx
+++ web/react/src/index.tsx
@@ -23,7 +23,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister(); // unregister first
 serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
